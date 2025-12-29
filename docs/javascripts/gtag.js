dd76a7a4592c7d5dcf470095e59da8f26fda0795
梁典典 <413153189@qq.com>
2025-12-29 16:57:37 +0800

    Add Firebase Analytics integration

---
+++ docs/javascripts/gtag.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAholiIQTLfGUB23uzJ7AdbM8rCh19K3w",
+  authDomain: "flutterx-doc.firebaseapp.com",
+  projectId: "flutterx-doc",
+  storageBucket: "flutterx-doc.firebasestorage.app",
+  messagingSenderId: "883380844982",
+  appId: "1:883380844982:web:747049ce741d4063ae6917",
+  measurementId: "G-G3WJSBLPN0"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
